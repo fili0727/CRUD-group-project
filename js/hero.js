6df552a60de2cc7419d235009d4c04064c0fd80e
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-05-02 14:33:01 +0200

    await

--- js/hero.js
+++ js/hero.js
@@ -1,7 +1,12 @@
 "use strict";
 
 // import data
-import { getUsers, createUser, deleteUser, updateUser } from "./rest-service.js";
+import {
+  getUsers,
+  createUser,
+  deleteUser,
+  updateUser,
+} from "./rest-service.js";
 
 import {} from "./helpers.js";
 
@@ -15,11 +20,19 @@ function initApp() {
   updateUsersGrid();
 
   // event listener
-  document.querySelector("#btn-create-user").addEventListener("click", showCreateUserDialog);
-  document.querySelector("#form-create-user").addEventListener("submit", createUserClicked);
+  document
+    .querySelector("#btn-create-user")
+    .addEventListener("click", showCreateUserDialog);
+  document
+    .querySelector("#form-create-user")
+    .addEventListener("submit", createUserClicked);
 
-  document.querySelector("#input-search").addEventListener("keyup", inputSearchChanged);
-  document.querySelector("#input-search").addEventListener("search", inputSearchChanged);
+  document
+    .querySelector("#input-search")
+    .addEventListener("keyup", inputSearchChanged);
+  document
+    .querySelector("#input-search")
+    .addEventListener("search", inputSearchChanged);
 }
 
 // events
@@ -38,7 +51,9 @@ function inputSearchChanged(event) {
 function searchUsers(search) {
   search = search.toLowerCase();
 
-  const results = users.filter(user => user.name.toLowerCase().includes(search));
+  const results = users.filter(user =>
+    user.name.toLowerCase().includes(search)
+  );
   return results;
 }
 
@@ -73,19 +88,28 @@ function showUser(userObject) {
   document.querySelector("#users").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
 
   // add event listeners to .btn-delete and .btn-update
-  document.querySelector("#users article:last-child .btn-delete").addEventListener("click", deleteClicked);
-  document.querySelector("#users article:last-child .btn-update").addEventListener("click", updateClicked);
+  document
+    .querySelector("#users article:last-child .btn-delete")
+    .addEventListener("click", deleteClicked);
+  document
+    .querySelector("#users article:last-child .btn-update")
+    .addEventListener("click", updateClicked);
 
   // called when delete button is clicked
   function deleteClicked() {
     console.log("Delete button clicked");
     document.querySelector("#dialog-delete-user").showModal();
-    document.querySelector("#dialog-delete-user-alias").textContent = userObject.alias;
-    document.querySelector("#form-delete-user").setAttribute("data-id", userObject.id);
+    document.querySelector("#dialog-delete-user-alias").textContent =
+      userObject.alias;
+    document
+      .querySelector("#form-delete-user")
+      .setAttribute("data-id", userObject.id);
     document.querySelector("#btn-no").addEventListener("click", function () {
       document.querySelector("#dialog-delete-user").close();
     });
-    document.querySelector("#form-delete-user").addEventListener("submit", deleteUserClicked);
+    document
+      .querySelector("#form-delete-user")
+      .addEventListener("submit", deleteUserClicked);
   }
 
   // called when update button is clicked
@@ -97,8 +121,12 @@ function showUser(userObject) {
     document.querySelector("#name-update").value = userObject.name;
     document.querySelector("#powers-update").value = userObject.powers;
     document.querySelector("#image-update").value = userObject.image;
-    document.querySelector("#form-update-user").setAttribute("data-id", userObject.id);
-    document.querySelector("#form-update-user").addEventListener("submit", updateUserClicked);
+    document
+      .querySelector("#form-update-user")
+      .setAttribute("data-id", userObject.id);
+    document
+      .querySelector("#form-update-user")
+      .addEventListener("submit", updateUserClicked);
   }
 }
 
@@ -107,7 +135,7 @@ async function deleteUserClicked(event) {
   const form = event.target;
   const id = form.getAttribute("data-id");
   console.log(id);
-  const response = deleteUser(id);
+  const response = await deleteUser(id);
   if (response.ok) updateUsersGrid();
   form.reset();
   document.querySelector("#dialog-delete-user").close();
@@ -135,7 +163,7 @@ async function createUserClicked(event) {
   const powers = document.querySelector("#powers-input").value;
   const name = document.querySelector("#name-input").value;
   const img = document.querySelector("#image-input").value;
-  const response = createUser(name, alias, powers, img);
+  const response = await createUser(name, alias, powers, img);
   if (response.ok) updateUsersGrid();
   const form = event.target;
   form.reset();
