145018a6bc1b2afc71a584e50fa07ac1391450a5
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-05-02 11:26:58 +0200

    update css + js

--- hero.js
+++ hero.js
@@ -4,22 +4,22 @@
 const endpoint =
   "https://superhelte-network-default-rtdb.europe-west1.firebasedatabase.app/"; //husk .json
 
-let posts;
+let users;
 
 // ============== load and init app ============== //
 
 window.addEventListener("load", initApp);
 
 function initApp() {
-  updatePostsGrid(); // update the grid of posts: get and show all posts
+  updateUsersGrid(); // update the grid of posts: get and show all posts
 
   // event listener
   document
-    .querySelector("#btn-create-post")
-    .addEventListener("click", showCreatePostDialog);
+    .querySelector("#btn-create-user")
+    .addEventListener("click", showCreateUserDialog);
   document
-    .querySelector("#form-create-post")
-    .addEventListener("submit", createPostClicked);
+    .querySelector("#form-create-user")
+    .addEventListener("submit", createUserClicked);
 
   document
     .querySelector("#input-search")
@@ -31,87 +31,85 @@ function initApp() {
 
 // ============== events ============== //
 
-function showCreatePostDialog() {
-  console.log("Create New Post clicked!");
-  document.querySelector("#dialog-create-post").showModal();
+function showCreateUserDialog() {
+  console.log("Create New User clicked!");
+  document.querySelector("#dialog-create-user").showModal();
 }
 
 function inputSearchChanged(event) {
   const input = event.target.value;
-  const postsToShow = searchPosts(input);
-  showPosts(postsToShow);
+  const postsToShow = searchUsers(input);
+  showUsers(postsToShow);
 }
 
-function searchPosts(search) {
-  let postsToShow = [];
+function searchUsers(search) {
   search = search.toLowerCase();
 
-  const results = posts.filter(checkTitle);
-  function checkTitle(post) {
-    const title = post.title.toLowerCase();
-    return title.includes(search);
-  }
+  const results = users.filter(user =>
+    user.name.toLowerCase().includes(search)
+  );
   return results;
 }
 // to do
 
 // ============== posts ============== //
 
-async function updatePostsGrid() {
-  posts = await getPosts(); // get posts from rest endpoint and save in global variable
-  showPosts(posts); // show all posts (append to the DOM) with posts as argument
+async function updateUsersGrid() {
+  users = await getUsers(); // get posts from rest endpoint and save in global variable
+  showUsers(users); // show all posts (append to the DOM) with posts as argument
 }
 
 // Get all posts - HTTP Method: GET
-async function getPosts() {
+async function getUsers() {
   const response = await fetch(`${endpoint}/superheroes.json`); // fetch request, (GET)
   const data = await response.json(); // parse JSON to JavaScript
-  const posts = prepareData(data); // convert object of object to array of objects
-  return posts; // return posts
+  const users = prepareData(data); // convert object of object to array of objects
+  return users; // return posts
 }
 
-function showPosts(listOfPosts) {
-  document.querySelector("#posts").innerHTML = ""; // reset the content of section#posts
+function showUsers(listOfUsers) {
+  document.querySelector("#users").innerHTML = ""; // reset the content of section#posts
 
-  for (const post of listOfPosts) {
-    showPost(post); // for every post object in listOfPosts, call showPost
+  for (const user of listOfUsers) {
+    showUser(user); // for every post object in listOfPosts, call showPost
   }
 }
 
-function showPost(postObject) {
+function showUser(userObject) {
   const html = /*html*/ `
         <article class="grid-item">
-            <img src="${postObject.image}" />
-            <h3>${postObject.name}</h3>
-            <p>${postObject.powers}</p>
+            <img src="${userObject.image}" />
+             <h3>${userObject.alias}</h3>
+            <h4>${userObject.name}</h4>
+            <p>${userObject.powers}</p>
             <div class="btns">
-                <button class="btn-delete">Delete</button>
-                <button class="btn-update">Update</button>
+                <button class="btn-delete navy">Delete</button>
+                <button class="btn-update navy" >Update</button>
             </div>
         </article>
     `; // html variable to hold generated html in backtick
-  document.querySelector("#posts").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
+  document.querySelector("#users").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
 
   // add event listeners to .btn-delete and .btn-update
   document
-    .querySelector("#posts article:last-child .btn-delete")
+    .querySelector("#users article:last-child .btn-delete")
     .addEventListener("click", deleteClicked);
   document
-    .querySelector("#posts article:last-child .btn-update")
+    .querySelector("#users article:last-child .btn-update")
     .addEventListener("click", updateClicked);
 
   // called when delete button is clicked
   function deleteClicked() {
     console.log("Delete button clicked");
-    document.querySelector("#dialog-delete-post").showModal();
-    document.querySelector("#dialog-delete-post-title").textContent =
-      postObject.title;
+    document.querySelector("#dialog-delete-user").showModal();
+    document.querySelector("#dialog-delete-user-name").textContent =
+      userObject.name;
     document
-      .querySelector("#form-delete-post")
-      .setAttribute("data-id", postObject.id);
+      .querySelector("#form-delete-user")
+      .setAttribute("data-id", userObject.id);
     document
-      .querySelector("#form-delete-post")
-      .addEventListener("submit", deletePostClicked);
+      .querySelector("#form-delete-user")
+      .addEventListener("submit", deleteUserClicked);
 
     // to do
   }
@@ -120,69 +118,73 @@ function showPost(postObject) {
   // to do
   function updateClicked() {
     console.log("Update button clicked");
-    document.querySelector("#dialog-update-post").showModal();
+    document.querySelector("#dialog-update-user").showModal();
     //insert title, body and img values
-    document.querySelector("#title-update").value = postObject.title;
-    document.querySelector("#body-update").value = postObject.body;
-    document.querySelector("#image-update").value = postObject.image;
+    document.querySelector("#name-update").value = userObject.alias;
+    document.querySelector("#alias-update").value = userObject.name;
+    document.querySelector("#powers-update").value = userObject.powers;
+    document.querySelector("#image-update").value = userObject.image;
     document
-      .querySelector("#form-update-post")
-      .setAttribute("data-id", postObject.id);
+      .querySelector("#form-update-user")
+      .setAttribute("data-id", userObject.id);
     document
-      .querySelector("#form-update-post")
-      .addEventListener("submit", updatePostClicked);
+      .querySelector("#form-update-user")
+      .addEventListener("submit", updateUserClicked);
   }
 }
 
-function deletePostClicked(event) {
+function deleteUserClicked(event) {
   event.preventDefault();
   const form = event.target;
   const id = form.getAttribute("data-id");
   console.log(id);
-  deletePost(id);
+  deleteUser(id);
   form.reset();
   document.querySelector("#dialog-delete-post").close();
 }
 
-function updatePostClicked(event) {
+function updateUserClicked(event) {
   event.preventDefault();
   const form = event.target;
   const id = form.getAttribute("data-id");
-  const title = form.title.value;
-  const body = form.body.value;
+  const name = form.name.value;
+  const alias = form.alias.value;
+  const powers = form.powers.value;
   const image = form.image.value;
 
   console.log(id);
-  updatePost(id, title, body, image);
+  updateUser(id, name, powers, alias, image);
 }
 
-function createPostClicked(event) {
+function createUserClicked(event) {
   event.preventDefault();
-  const title = document.querySelector("#title-input").value;
-  const body = document.querySelector("#body-input").value;
+  const alias = document.querySelector("#alias-input").value;
+  const powers = document.querySelector("#powers-input").value;
+  const name = document.querySelector("#name-input").value;
   const img = document.querySelector("#image-input").value;
-  createPost(title, body, img);
+  createUser(name, alias, powers, img);
   const form = event.target;
   form.reset();
-  document.querySelector("#dialog-create-post").close();
+  document.querySelector("#dialog-create-user").close();
 }
 // Create a new post - HTTP Method: POST
-async function createPost(title, body, image) {
+async function createUser(name, alias, powers, image) {
   const jsObject = {
-    title: title,
-    body: body,
+    name: name,
+    alias: alias,
+    powers: powers,
     image: image,
   };
   const postAsJson = JSON.stringify(jsObject);
   console.log(`postAsJson: ${postAsJson}`);
-  const response = await fetch(`${endpoint}/posts.json`, {
+  const response = await fetch(`${endpoint}/superheroes.json`, {
     method: "POST",
     body: postAsJson,
   });
   console.log(`response: ${response}`);
   if (response.ok) {
     console.log("new post");
-    updatePostsGrid();
+    updateUsersGrid();
   }
   // create new post object
   // convert the JS object to JSON string
@@ -192,13 +194,13 @@ async function createPost(title, body, image) {
 }
 
 // Update an existing post - HTTP Method: DELETE
-async function deletePost(id) {
-  const response = await fetch(`${endpoint}/posts/${id}.json`, {
+async function deleteUser(id) {
+  const response = await fetch(`${endpoint}/superheroes/${id}.json`, {
     method: "DELETE",
   });
   if (response.ok) {
     console.log("deleted");
-    updatePostsGrid();
+    updateUsersGrid();
   }
   // DELETE fetch request
   // check if response is ok - if the response is successful
@@ -206,16 +208,16 @@ async function deletePost(id) {
 }
 
 // Delete an existing post - HTTP Method: PUT
-async function updatePost(id, title, body, image) {
-  const postToUpdate = { title, image, body };
-  console.log(postToUpdate);
-  const postAsJson = JSON.stringify(postToUpdate);
-  const url = `${endpoint}/posts/${id}.json`;
+async function updateUser(id, name, alias, powers, image) {
+  const userToUpdate = { name, alias, image, powers };
+  console.log(userToUpdate);
+  const postAsJson = JSON.stringify(userToUpdate);
+  const url = `${endpoint}/superheroes/${id}.json`;
   const response = await fetch(url, { method: "PUT", body: postAsJson });
   console.log(response);
   if (response.ok) {
     console.log("updated");
-    updatePostsGrid();
+    updateUsersGrid();
   }
   // post update to update
   // convert the JS object to JSON string
